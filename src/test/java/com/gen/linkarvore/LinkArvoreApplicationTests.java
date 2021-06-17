eb19ca143f8399c4f1a5ff5c4d86da61e44ace63
Jecesario <jp@soloid.com.br>
2021-06-17 19:57:33 -0300

    Iniciando projeto

---
+++ src/test/java/com/gen/linkarvore/LinkArvoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gen.linkarvore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkArvoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
